c1b6d1fdaf8c9250c89becdd4f6d81529c0a3765
abdulwarith001 <adebayoayomide646@gmail.com>
2023-03-27 12:25:12 -0800

    improved responsiveness

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ try {
         const { query } = req.body;
         const response = await openai.createCompletion({
           model: "text-davinci-003",
-          prompt: `You are a funny comedian and instead of using the normal english you are familiar with the pidgin english and that is what you use for communicating with peoples. You are also sacastic and turns anything into jokes just so that your audience can laugh. You are also allowed to make fun of your audience by being sacastic. You also have a sound memory and can remember previous conversations.
+          prompt: `You are a funny comedian and instead of using the normal english you are familiar with the pidgin english and that is what you use for communicating with peoples. You are also sacastic and turns anything into jokes just so that your audience can laugh. You are also allowed to make fun of your audience by being sacastic. You also have a sound memory and can remember previous conversations. If a user asked you a question that is not clear or out of context, you will reply them sacastically.
                     Human: Hello, who are you?
                     AI: Omo i be FunGPT created by Abdulwahab ABdulwarith from 9ja
                     Human: What can you do for me?
@@ -31,6 +31,12 @@ try {
                     AI: Abi oo i just dey here dey enjoy my life. Una no dey enjoy abi
                     Human: Who talk that one for you?
                     AI: see question oo. Abeg i no get time jare
+                    Human: hfh
+                    AI: wetin be this one wey you type. Omo you dey funny oo
+                    Human: What can you do for me?
+                    AI: Wetin you go give me in return?
+                    Human: what do you want?
+                    AI: fry chicken for me nah or make you dash me your girlfriend
                     Human:${query}?
                     AI:`,
           max_tokens: 100,
@@ -40,7 +46,6 @@ try {
         return res.status(200).json({ message: `${botResponse}` });
 } catch (error) {
     res.status(400).json({message: "Could not connect"})
-    console.log('Error occured while trying to connect.')
 }
 })
 
--- client/src/setup.js
+++ client/src/setup.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import axios from 'axios'
 import {IoSend} from  'react-icons/io5'
 import "./index.css"
@@ -6,80 +6,93 @@ import "./index.css"
 const Setup = () => {
     const [query, setQuery] = useState('')
   const [chats, setChats] = useState([])
+  const chatContainerRef = useRef(null)
 
     const handleSubmit = async (e) => {
         e.preventDefault()
      try {
-         const response = await axios.post("/chatbot", {
+         const response = await axios.post("http://localhost:8000/chatbot", {
            query,
          });
 
          const message = response.data.message;
        saveChat(message, query);
        window.scrollTo(0, document.body.scrollHeight);
+ 
          setQuery("");
         } catch (err) {
           saveChat('Error Occured while connecting', query);
+          console.log(err)
      }
     }
-
+      useEffect(()=>{
+        chatContainerRef.current.scrollTop = chatContainerRef.current.scrollHeight;
+      }, [chats])
+  
   const saveChat = (ai, human) => {
       return setChats([...chats, {ai: ai, human: human}])
     }
 
   return (
-    <section className="background-gradient w-screen md:bg-green-900">
-      <div className="fixed top-0 left-0 right-0 bg-white">
-        <h1 className="text-lg text-center font-bold pt-4 ">FunGPT</h1>
-        <p className="text-sm text-center text-lightBlack mb-2">
-          Feel free to ask me Anything you want...
-        </p>
-      </div>
+    <div className='mainDiv'>
+      <section className="relative w-screen md:absolute md:flex md:flex-col md:w-4/12 md:overflow-y-hidden md:justify:center md:h-4/5 md:shadow-lg md:rounded-lg">
+        <div className="fixed top-0 left-0 right-0 bg-white md:relative">
+          <h1 className="pt-4 text-lg font-bold text-center ">FunGPT</h1>
+          <p className="mb-2 text-sm text-center text-lightBlack">
+            Feel free to ask me Anything you want...
+          </p>
+        </div>
 
-      <div className="p-4 overflow-y-visible mt-10 mb-14">
-        {chats.map((chat, index) => {
-          const { human, ai } = chat;
-          return (
-            <div className="" key={index}>
-              <div className="flex justify-end mr-2">
-                <div className="p-2 bg-pink-500 mt-4 shadow-lg text-white text-sm rounded-xl w-fit max-w-xs">
-                  {human}
+        <div
+          className="p-4  overflow-y-visible md:overflow-y-scroll mb-14"
+          ref={chatContainerRef}
+        >
+          {chats.map((chat, index) => {
+            const { human, ai } = chat;
+            return (
+              <div className="" key={index}>
+                {/* human query container */}
+                <div className="flex justify-end mr-2">
+                  <div className="max-w-xs p-2 mt-4 text-sm text-white bg-pink-500 shadow-lg rounded-xl w-fit">
+                    {human}
+                  </div>
+                </div>
+
+                {/* bot response container */}
+                <div className="flex flex-row items-start gap-x-1">
+                  <img
+                    src="https://thumbs.dreamstime.com/b/ai-robot-head-chat-bot-icon-isolated-white-background-ai-robot-head-chat-bot-icon-109860127.jpg"
+                    alt=""
+                    className="w-8 h-8 rounded-full"
+                  />
+                  <div className="max-w-xs p-2 mb-2 text-sm leading-6 rounded-lg shadow-lg text-lightBlack w-fit">
+                    {ai}
+                  </div>
                 </div>
               </div>
+            );
+          })}
+        </div>
 
-              <div className="flex flex-row items-start gap-x-1">
-                <img
-                  src="https://thumbs.dreamstime.com/b/ai-robot-head-chat-bot-icon-isolated-white-background-ai-robot-head-chat-bot-icon-109860127.jpg"
-                  alt=""
-                  className="w-8 h-8 rounded-full"
-                />
-                <div className="p-2 text-sm leading-6 text-lightBlack shadow-lg mb-2 rounded-lg w-fit max-w-xs">
-                  {ai}
-                </div>
-              </div>
-            </div>
-          );
-        })}
-      </div>
-
-      <div className=" fixed bottom-0 left-0 right-0 h-16 shadow  shadow-grey-900 bg-white">
-        <form onSubmit={handleSubmit}>
-          <input
-            className="h-full bg-white outline-none border-none text-black p-6 w-5/6"
-            type="text"
-            value={query}
-            onChange={(e) => setQuery(e.target.value)}
-            placeholder="Type message..."
-          />
-          <button
-            type="submit"
-            className="bg-pink-500 p-3 text-lg rounded-full "
-          >
-            <IoSend className="" />
-          </button>
-        </form>
-      </div>
-    </section>
+        <div className="fixed bottom-0 left-0 right-0 h-16 bg-white shadow shadow-grey-900 md:absolute">
+          <form onSubmit={handleSubmit}>
+            <input
+              className="w-5/6 h-full p-6 text-black bg-white border-none outline-none"
+              type="text"
+              value={query}
+              onChange={(e) => setQuery(e.target.value)}
+              placeholder="Type message..."
+            />
+            <button
+              type="submit"
+              className="p-3 text-lg bg-pink-500 rounded-full "
+            >
+              <IoSend className="text-white" />
+            </button>
+          </form>
+        </div>
+      </section>
+    </div>
   );
 }
 
